fcb641d5bcbe7f1c522ca7bdd5db04b2fea91fb8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2016-11-14 21:44:00 +0000

    Added header file for the file handler

---
+++ fileHandler.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#ifndef _FILEHANDLER_H
+#define _FILEHANDLER_H
+
+using namespace std;
+
+class FileHandler{
+	public:
+		static string[] readFile(string filename)
+		static void writeFile(string[] linse, string filename);
+}
