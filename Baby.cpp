97eae0be21c6b71d86a5d8978a518a11974a09e4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2016-11-15 21:58:33 +0000

    added baby.cpp

---
+++ Baby.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Baby.h"
+
+void Baby::execute(){
+	switch(function){
+		case 0:
+			controlInstruction = operand;
+			break;
+		case 1:
+			controlInstruction += operand;
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+	}
+}
