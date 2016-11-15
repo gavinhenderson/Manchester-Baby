6b0b4ea2dbb1fc49e5ce652a327f5006213e599c
Glynja <ignoranceuk@gmail.com>
2016-11-15 19:24:30 +0000

    typo

--- BinaryConversion.h
+++ BinaryConversion.h
@@ -12,11 +12,11 @@ class BinaryConversion{
 	public:
 		//Constructor that will create a binary conversion and convert the int
 		//that is passed in and save the result to binaryString
-		void DecimalConversion(long decimal, char *binary);
+		void decimalConversion(long decimal, char *binary);
 		
 		//Constructor that will create a binary conversion and convert the string
 		//that is passed in and save the result to int
-		void BinaryConversion(string &bin);
+		void binaryConversion(string &bin);
 		
 		//Returns the binaryString
 		string getBinaryString();
