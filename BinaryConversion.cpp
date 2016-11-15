b9ffacb8d09046f6b7740d9a7f76602977662659
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2016-11-15 21:34:14 +0000

    USE GIT PROPERLY YOU FUDS

--- BinaryConversion.cpp
+++ BinaryConversion.cpp
@@ -1,60 +1,42 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
 #include "BinaryConversion.h"
+
 using namespace std;
 
-// constructor
-BinaryConversion::BinaryConversion()
+//Constructor that takes in a long and coverts it to string
+BinaryConversion::BinaryConversion(long decimal)
 {
-  binaryInt = 0;
-  binaryString = "";
-}
+    //Initialise variables
+    char binary[80];
+	int k = 0;
+	int n = 0;
+    int remain;
+    char temp[80];
 
-// destructor
-BinaryConversion::~BinaryConversion()
-{
-
-}
-
-// method to convert decimal to binary
-void BinaryConversion::decimalConversion(long decimal, char *binary)
-{
-    // variables
-      int k = 0;
-      int n = 0;
-      int remain;
-      char temp[80];
-
-      // If user enters a negative number, prompts user to enter a positive number instead
-      // and exits the program
-      if (decimal < 0)
-      {      
-        cout << " Please enter a positive number." << endl;
-        exit(0);
-      }
-
-      do 
-      { 
-        remain = decimal%2;
+    do{
+		remain = decimal%2;
         decimal = decimal/2;
         // Converts 0 or 1 to char
         temp[k] = remain + '0';
         k++;
-      } while (decimal > 0);
+	}while (decimal > 0);
 
-      // null terminator
-      temp[k++] = '\0';
+    // null terminator
+    temp[k++] = '\0';
 
-      while (k >= 0)
-        binary[n++] = temp[--k];
-      binary[n-1] = 0;
+    while (k >= 0){
+    	binary[n++] = temp[--k];
+    }
+    binary[n-1] = 0;
+	binaryString = temp;
+	
+	//Reverse binaryString
+	reverse(binaryString.begin(), binaryString.end());
 
-      binaryString = temp;
 }
 
-// converts binary to decimal
-unsigned BinaryConversion::binaryConversion(string &bin)
+//Constructor that takes in a binary number and converts it to an integer
+BinaryConversion::BinaryConversion(string bin)
 {
     unsigned result=0;
     for (unsigned a=0;a<bin.size();a++){
@@ -65,40 +47,32 @@ unsigned BinaryConversion::binaryConversion(string &bin)
     }
 
     binaryInt = result;
-
-    return result;
 } 
-
 
 // Returns binaryString
 string BinaryConversion::getBinaryString()
 {
-  return binaryString;
+	return binaryString;
 }
 
 // Returns binaryInt
-int BinaryConversion::getBinaryInt()
+unsigned BinaryConversion::getBinaryInt()
 {
-  return binaryInt;
+	return binaryInt;
 }
 
-/*
+//TESTER
+
 int main()
 {
-    BinaryConversion* binCon = new BinaryConversion();
-    string bin;
-    char binary[80];
-
-    //4294967295
-    binCon->decimalConversion(4294967295, binary);
-    cout << binCon->getBinaryString() << endl;
-
-    cout << "Please input a binary number (up to "<<(sizeof(unsigned)*8)<<" bits): ";
-    getline(cin, bin);
-    cout << "The result is: " << binCon->binaryConversion(bin) << endl;
-
+    BinaryConversion* binCon = new BinaryConversion("11111111111111111111111111111111");
     cout << binCon->getBinaryInt() << endl;
-
+    
+    BinaryConversion* binCon2 = new BinaryConversion(4294967295);
+    cout << binCon2->getBinaryString() << endl;
+    
+    delete binCon;
+    delete binCon2;
+    
     return 0;
 }
-*/
